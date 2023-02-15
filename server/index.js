2078a5814a0c74e744d05a24f43cc606afdc6bfc
Roudrini Mukherjee <89720737+RM-2002@users.noreply.github.com>
2023-02-15 11:17:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,10 +7,21 @@ import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
-const PORT = 9000;
+const PORT = process.env.PORT || 9000;
 
 const app = express();
+
+/* To prevent CORS */
+app.options("*", cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(cors());
+
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
